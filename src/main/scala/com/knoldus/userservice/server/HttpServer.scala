e92729aba2505f457a49884bd2bb20f1a0ba5737
swantikag <swantika.gupta@knoldus.in>
2019-10-25 14:55:07 +0530

    Server file updated

--- src/main/scala/com/knoldus/userservice/server/HttpServer.scala
+++ src/main/scala/com/knoldus/userservice/server/HttpServer.scala
@@ -17,7 +17,7 @@ trait HttpServer extends UserServiceConfig {
   implicit def ec : ExecutionContext
   val routes : Route
   val bindingFuture = Http().bindAndHandle(routes, httpHost, httpPort)
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  println(s"Server online at http://localhost:8088/\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
   bindingFuture
     .flatMap(_.unbind()) // trigger unbinding from the port
